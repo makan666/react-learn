710095f0d4bbf40875db3de40400f8d4d0eedf4f
makan <makan@makanMacBook-Pro.local>
2018-03-17 18:28:00 +0800

    add contact page style

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,35 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import {getTrialCarList} from '../../model/actions/trialCars';
+import { List } from 'antd-mobile';
+const Item = List.Item;
+import './Contact.scss'
 
 class Contact extends React.Component {
-  static propTypes = {
-    trialCars: PropTypes.object
-  };
-
-  componentDidMount() {
-    this.props.getTrialCarList();
-  }
 
   render() {
-    const trialCars = this.props.trialCars;
-    if (trialCars && trialCars.data) {
-      const list = trialCars.data.map((item, index) => {
-        return <div key={index}>{item.displayName}</div>
-      })
-      return <div>{list}</div>
-    }
-    return <div>加载中。。。</div>;
+    return (
+        <div className='contact-page'>
+          <List className=''>
+            <Item thumb={require('../../assets/img/contact/my-favorite.svg')} arrow="horizontal" onClick={() => {}}>
+              我的收藏
+            </Item>
+          </List>
+          <List className='contact-page'>
+            <Item className='border-bottom' thumb={require('../../assets/img/contact/car-friends.svg')} onClick={() => {}} arrow="horizontal">
+              好有车
+            </Item>
+            <Item thumb={require('../../assets/img/contact/app-account.svg')} onClick={() => {}} arrow="horizontal">
+              应用号
+            </Item>
+          </List>
+        </div>
+    )
   }
 }
 
-const mapStateToProps = (state) => ({
-  trialCars: state.trialCars
-});
-
-const mapDispatchToProps = {
-  getTrialCarList
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Contact);
+export default Contact;
