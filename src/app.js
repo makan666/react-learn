938dc9f1770473a03bdd7741b5c475396c8156ab
makan <makan@makanMacBook-Pro.local>
2018-03-19 11:29:06 +0800

    modified app.js

--- src/app.js
+++ src/app.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import store from './model';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import TrialCars from './components/TrialCars/TrialCars';
 import Tabs from './components/Tabs/Tabs';
 
 class App extends React.Component {
-  render(){
-    return(
-      <Provider store={store}>
-        <div>
-          <Router>
-            <div>
-              <Route exact path="/" component={Tabs}/>
-              <Route exact path="/trialCars" component={TrialCars}/>
-            </div>
-          </Router>
-        </div>
-      </Provider>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <Router>
+                    <div>
+                        <Route exact path="/" component={Tabs}/>
+                        <Route exact path="/trial-cars" component={TrialCars}/>
+                    </div>
+                </Router>
+            </Provider>
+        );
+    }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App/>, document.getElementById('app'));
 
 // 热替换HMR，需要加入这段代码才会进行生效
 if (module.hot) {
-  module.hot.accept();
+    module.hot.accept();
 }
