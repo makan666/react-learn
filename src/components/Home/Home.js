d413c5516755e7ab1e56b7e2a781fa2ed15b7df8
makan <makan@makanMacBook-Pro.local>
2018-03-19 11:29:32 +0800

    home page add push

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import {connect} from 'react-redux';
 import {getSubscriptions} from '../../model/actions/subscriptions';
 import { List } from 'antd-mobile';
@@ -19,16 +20,31 @@ class Home extends React.Component {
         this.props.getSubscriptions();
     }
 
+    pushTo(name) {
+        switch (name) {
+            case '车商城':
+                return '';
+            case '试驾':
+                return '/trial-cars';
+            case '车头条':
+                return '';
+            case '活动':
+                return '';
+        }
+    }
+
     render() {
         const subscriptions = this.props.subscriptions;
         if (subscriptions && subscriptions.data) {
             const list = subscriptions.data.map((item, index) => {
                 return (
-                    <Item key={index} extra={moment(item.latestMsgAt).format('YYYY-MM-DD')}
-                          align="top" thumb={require('../../'+item.logo)} multipleLine>
-                        {item.name}
-                        <Brief>{item.latestMsg}</Brief>
-                    </Item>
+                    <Link to={this.pushTo(item.name)} key={index}>
+                        <Item extra={moment(item.latestMsgAt).format('YYYY-MM-DD')}
+                              align="top" thumb={require('../../'+item.logo)} multipleLine onClick={() => {}}>
+                            {item.name}
+                            <Brief>{item.latestMsg}</Brief>
+                        </Item>
+                    </Link>
                 )
 
             });
