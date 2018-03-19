77dc910b4f4a63eac889c4e23c472bf0177430f3
makan <makan@makanMacBook-Pro.local>
2018-03-19 13:18:29 +0800

    add trialCars page

--- src/components/TrialCars/TrialCars.js
+++ src/components/TrialCars/TrialCars.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import { NavBar, Icon } from 'antd-mobile';
 import {getTrialCarList} from '../../model/actions/trialCars';
+import './TrialCars.scss'
+import SearchBar from '../SearchBar/SearchBar';
 
 class TrialCars extends React.Component {
   static propTypes = {
@@ -12,13 +15,40 @@ class TrialCars extends React.Component {
     this.props.getTrialCarList();
   }
 
+  getImgUrl(key) {
+    return 'http://qiniu-cdn.carhot.cn/' + key + '?imageView/2/w/176/h/132'
+  }
+
   render() {
     const trialCars = this.props.trialCars;
     if (trialCars && trialCars.data) {
       const list = trialCars.data.map((item, index) => {
-        return <div key={index}>{item.displayName}</div>
+          return (
+              <div key={index} className='item'>
+                <img className="car-img" src={this.getImgUrl(item.cover)}/>
+                <div className="car-info">
+                  <div className="display-name">{item.displayName}</div>
+                  <div className="subtitle"><span className="price">{item.price}</span>元享试驾，{item.subTitle}</div>
+                  <div className="car-shop">{item.carShopName}</div>
+                </div>
+              </div>
+          )
       })
-      return <div>{list}</div>
+        return (
+            <div className='trial-cars'>
+              <NavBar
+                  mode="dark"
+                  icon={<Icon type="left" />}
+                  leftContent="返回"
+                  onLeftClick={() => this.props.history.goBack()}
+                  rightContent={[
+                    <Icon key="0" type="ellipsis" style={{ marginRight: '16px' }} />
+                  ]}
+              >试驾</NavBar>
+              <SearchBar placeholder='搜索'></SearchBar>
+              <div>{list}</div>
+            </div>
+        )
     }
     return <div>加载中。。。</div>;
   }
